dd526f723cf7b2c615e72f1b8025bafc6ff2e595
barasa001 <s.wanyonyi@alustudent.com>
2024-04-10 13:46:24 +0300

    made changes

---
+++ server.js
@@ -0,0 +1,36 @@
+// Import required module
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Create Express app
+const app = express();
+
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080;
+
+//log requests
+app.use(morgan('tiny'))
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}))
+
+//set view engine
+app.set("view engine", "ejs")
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img', express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js', express.static(path.resolve(__dirname,"assets/js")))
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Employee Management App!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
