1d2e4ba7a5cbaeab25cf9aa910c80a5e6608e11f
barasa001 <s.wanyonyi@alustudent.com>
2024-04-18 22:57:31 +0300

    more changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 $("#add_employee").submit(function(event){
     alert("Data Inserted Successfully!");
-})
+});
 
 $("#update_employee").submit(function(event){
     event.preventDefault();
@@ -12,13 +12,19 @@ $("#update_employee").submit(function(event){
         data[field.name] = field.value;
     });
 
+    console.log("Form data:", formData);
+    console.log("Serialized data:", data);
+
     var request = {
         url: `http://localhost:3000/api/employees/${data.id}`,
         method: "PUT",
-        data: data
+        data: data // Send form data as JSON
     };
 
+    console.log("Update request:", request);
+
     $.ajax(request).done(function(response){
+        console.log("Update response:", response);
         alert("Data Updated Successfully!");
     });
 });
@@ -28,17 +34,21 @@ if(window.location.pathname == "/"){
     $ondelete.click(function(){
         var id = $(this).attr("data-id")
 
+        console.log("Delete ID:", id);
+
         var request = {
             "url" : `http://localhost:3000/api/employees/${id}`,
             "method" : "DELETE"
-        }
+        };
+
+        console.log("Delete request:", request);
 
         if(confirm("Do you really want to delete this record?")){
             $.ajax(request).done(function(response){
+                console.log("Delete response:", response);
                 alert("Data Deleted Successfully!");
                 location.reload();
-            })
+            });
         }
-
-    })
+    });
 }
--- server/database/connection.js
+++ server/database/connection.js
@@ -4,8 +4,6 @@ const connectDB = async () => {
     try{
         // mongodb connection string
         const con = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
         })
 
         console.log(`MongoDB connected : ${con.connection.host}`);
